0007d823017bbd806d643d6ab31750c7047d7606
chen <chen@test.com>
2021-01-17 00:43:26 +0800

    数据库兼容处理

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 为了兼容 mysql 5.7 以下
+        Schema::defaultStringLength(191);
     }
 }
