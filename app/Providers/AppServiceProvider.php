fad6ecb3c4196edabf7664c8003de388183bc786
chen <chen@test.com>
2021-03-04 23:26:02 +0800

    调整异常数据格式

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,20 @@ class AppServiceProvider extends ServiceProvider
     {
         // 为了兼容 mysql 5.7 以下
         //Schema::defaultStringLength(191);
+        app(\Dingo\Api\Exception\Handler::class)->register(function (\Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException $exception) {
+            return \Illuminate\Support\Facades\Response::make([
+                'code' => 401,
+                'msg'  => 'token 无效，请重新登录',
+                'data' => []
+            ], 401);
+        });
+
+        app(\Dingo\Api\Exception\Handler::class)->register(function (\Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException $exception) {
+            return \Illuminate\Support\Facades\Response::make([
+                'code' => 405,
+                'msg'  => '请求方式错误',
+                'data' => []
+            ], 405);
+        });
     }
 }
