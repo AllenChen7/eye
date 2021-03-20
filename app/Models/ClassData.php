d8886fb54ac88b52cf364d78e0028a4c1823f2ad
chen <chen@test.com>
2021-03-21 00:18:00 +0800

    修正数据格式

--- app/Models/ClassData.php
+++ app/Models/ClassData.php
@@ -102,6 +102,6 @@ class ClassData extends Model
                 break;
         }
 
-        return $query->select(['id'])->get()->pluck('id');
+        return $query->select(['id'])->get()->pluck('id')->toArray();
     }
 }
