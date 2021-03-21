39bdbba8a4a18bb2e9c9c316a17050cba034bcc6
chen <chen@test.com>
2021-03-21 12:03:57 +0800

    创建用户时新增角色

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -78,7 +78,8 @@ class AuthController extends ApiController
             'remark'                => 'nullable|max:200',
             'type'                  => [
                 'required', Rule::in(Common::typeArrKeys()),
-            ]
+            ],
+            'role_arr'              => 'nullable|array'
         ]);
         // 如果是校级的话需要指定省级-市级-县级并创建学校数据
         // 用户创建怎么处理 todo type 为 0 是为用户创建
@@ -204,6 +205,11 @@ class AuthController extends ApiController
         }
 
         if ($model->save()) {
+
+            if ($request->input('type') == Common::TYPE_ZONE) {
+                $model->assignRole($request->input('role_arr'));
+            }
+            
             return $this->successResponse();
         } else {
             return $this->errorResponse();
