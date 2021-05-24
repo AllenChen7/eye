06f95bc101d1183b908cb9020040a086448f872a
chen <1175195335@qq.com>
2021-05-24 14:36:11 +0800

    验光用户 log

--- database/migrations/2021_03_21_122227_add_plan_id_column_to_students_table.php
+++ database/migrations/2021_03_21_122227_add_plan_id_column_to_students_table.php
@@ -20,6 +20,7 @@ class AddPlanIdColumnToStudentsTable extends Migration
 
         Schema::table('student_logs', function (Blueprint $table) {
             $table->unsignedInteger('plan_id')->index()->comment('计划ID')->default(0)->after('status');
+            $table->unsignedTinyInteger('plan_status')->index()->comment('计划状态, 0未验光计划、1开启验光计划、2验光完毕、3验光废弃')->default(2)->after('status');
         });
     }
 
@@ -37,6 +38,7 @@ class AddPlanIdColumnToStudentsTable extends Migration
 
         Schema::table('student_logs', function (Blueprint $table) {
             $table->dropColumn('plan_id');
+            $table->dropColumn('plan_status');
         });
     }
 }
