e2e5788410c25c8baa7d14e61dd0461b09a8a0ad
chen <1175195335@qq.com>
2021-05-24 16:36:53 +0800

    desc

--- app/Http/Controllers/Api/DownloadController.php
+++ app/Http/Controllers/Api/DownloadController.php
@@ -20,7 +20,7 @@ class DownloadController extends ApiController
         $offset = $page <= 1 ? 0 : ($page - 1) * $limit;
         $list = Download::where([
             'user_id' => auth()->id()
-        ])->limit($limit)->offset($offset)->orderBy('id desc')->get();
+        ])->limit($limit)->offset($offset)->orderByDesc('id')->get();
 
         return $this->successResponse($list);
     }
